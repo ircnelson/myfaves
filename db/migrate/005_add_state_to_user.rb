0c9a91fa4a18b9a1e146f6fd2c67b3fc021384b8
Nelson Corrêa <ircnelson@gmail.com>
2009-03-11 12:22:44 -0400

    teste

--- db/migrate/005_add_state_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddStateToUser < ActiveRecord::Migration
-  def self.up
-  	add_column :users, :state, :string, :default => 'step_one'
-  end
-
-  def self.down
-  	remove_column :users, :state
-  end
-end
